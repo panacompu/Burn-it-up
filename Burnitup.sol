c295369b4c75beb578fdd41c913073ba1ea5d769
panacompu <88511450+panacompu@users.noreply.github.com>
2021-10-14 20:05:19 -0500

    Update Burnitup.sol

--- Burnitup.sol
+++ Burnitup.sol
@@ -6,10 +6,10 @@ import "https://github.com/panacompu/Burn-it-up/blob/main/BEP20Token.sol";
 contract CustomToken is  BEP20Token {
 
     constructor() {
-    _name = "Teste 6";
-    _symbol = "TT6";
-    _decimals = 6;
-    _totalSupply = 10000 * 10 ** 6;
+    _name = "Burn it up";
+    _symbol = "BIP";
+    _decimals = 18;
+    _totalSupply = 10000 * 10 ** 18;
     _balances[msg.sender] = _totalSupply;
 
     emit Transfer(address(0), msg.sender, _totalSupply);
